fdf3e0c1899ba610282c7c3bbe108342516c94f4
mvpsudarshan <mvpsudarshan@gmail.com>
2026-06-23 09:08:35 +0530

    Refactor app.py to improve code structure

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
 import cv2
 import numpy as np
+import mediapipe as mp
 import math
 
 # Session State for persistence
